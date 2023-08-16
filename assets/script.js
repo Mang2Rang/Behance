96f5218fc4b48b157513ff295d806bfbb5b8a6fb
Mang2Rang <skypopoioio@naver.com>
2023-08-16 17:28:39 +0900

    Behance

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView:7,
+  spaceBetween:20,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    
+  });
+
+const Click = document.querySelectorAll('.swiper-slide')
+
+Click.addEventListener('click',()=>{
+  
+
+})
+
