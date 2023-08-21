686bdc45475314f7293b959592e38bca0c4bcb62
Mang2Rang <skypopoioio@naver.com>
2023-08-21 09:34:11 +0900

    23.08.21

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,44 @@
 const swiper = new Swiper('.swiper', {
-  slidesPerView:7,
-  spaceBetween:20,
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-    
-  });
+  slidesPerView: 6,
+  spaceBetween: 20,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-const Click = document.querySelectorAll('.swiper-slide')
+  // // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-Click.addEventListener('click',()=>{
-  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+
+});
+
+const swSlide = document.querySelectorAll('.swiper-slide')
+const Show = document.querySelectorAll('.title')
+
+swSlide.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    if (item.classList.contains('selected')) {
+      item.classList.remove('selected')
+    } else {
+      swSlide.forEach(item => {
+        item.classList.remove('selected')
+      })
+      item.classList.add('selected')
+      Show.forEach(item2=>{
+        item2.classList.remove('show')
+      })
+    }
+    Show[index].classList.add('show')
+  })
 })
-
